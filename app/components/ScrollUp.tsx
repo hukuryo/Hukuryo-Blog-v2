32210b693883c70b0607e76224226c024f906d48
hukuryo <ryohei.1998.05.09@gmail.com>
2024-06-11 11:26:25 +0900

    トップページが表示できるようにした

--- app/components/ScrollUp.tsx
+++ app/components/ScrollUp.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
