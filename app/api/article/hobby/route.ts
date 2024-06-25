d5b2f8bd7df03750919b22cb4866b8215ea8cae0
hukuryo <ryohei.1998.05.09@gmail.com>
2024-06-25 22:16:36 +0900

    ✨ add feature: 記事を取得するAPIを作成

---
+++ app/api/article/hobby/route.ts
@@ -0,0 +1,15 @@
+import { client } from '@/app/lib/client';
+import { ArticleContent } from '@/app/types/article';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const articles = await client.get({
+    endpoint: 'articles',
+  });
+
+  const articlesByTypes: ArticleContent[] = articles.contents.filter(
+    (article: ArticleContent) => article.kinds[0] === 'hobby',
+  );
+
+  return NextResponse.json(articlesByTypes);
+}
