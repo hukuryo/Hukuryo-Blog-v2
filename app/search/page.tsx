138372a66c54b0e8a90f08401b08c0c44d1159a5
hukuryo <ryohei.1998.05.09@gmail.com>
2024-07-07 19:21:24 +0900

    🔥 refactor: 検索ページで表示する記事の一覧をコンポーネント化

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,50 +1,24 @@
-import { useRouter } from 'next/router';
-import { client } from '../lib/client';
-
 import { PageTitle } from '../components/PageTitle';
-import ArticleList from '../components/ArticleList';
-import { SideBar } from '@/app/components/SideBar';
-import { Layout } from '@/app/components/Layout';
+import SearchArticleList from '../components/SearchArticleList';
 import { PageTracking } from '@/app/components/PageTracking';
+import { Layout } from '@/app/components/Layout';
+import { SideBar } from '@/app/components/SideBar';
 import { ResponsiveProfile } from '@/app/components/ResponsiveProfile';
-import { ArticleContent } from '../types/article';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '記事を検索',
-  description: '記事を検索',
+  title: '趣味の記事一覧',
+  description: '趣味の記事一覧',
 };
 
-export default async function Page() {
-  // const router = useRouter();
-  const { s } = router.query;
-  const articles = await client.get({ endpoint: 'articles' });
-  const queryText: string = Array.isArray(s) ? s[0] : s || '';
-
-  const searchArticles = articles.filter((article: ArticleContent) =>
-    article.title.includes(queryText),
-  );
-
+export default function Page() {
   return (
     <>
-      {queryText.length === 0 ? (
-        <PageTracking pass={'tech'} pageTitle={`検索結果`} />
-      ) : (
-        <PageTracking pass={'tech'} pageTitle={`「${queryText}」の検索結果`} />
-      )}
+      <PageTracking pass={'search'} pageTitle={'記事を検索'} />
       <Layout>
-        {queryText.length === 0 ? (
-          <PageTitle title={`検索結果`} />
-        ) : (
-          <PageTitle title={`「${queryText}」の検索結果`} />
-        )}
-        {searchArticles.length === 0 && (
-          <>
-            <p>該当する記事は見つかりませんでした。</p>
-          </>
-        )}
+        <PageTitle title={'検索'} />
         <div className="md:flex justify-between">
-          <ArticleList pass={'tech'} />
+          <SearchArticleList pass={'search'} searchPass={'fff'} />
           <SideBar />
         </div>
         <ResponsiveProfile />
