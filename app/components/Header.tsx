3490907346a8b470761ec043943aeae4be37088d
hukuryo <ryohei.1998.05.09@gmail.com>
2024-07-27 10:29:42 +0900

    🔥 refactoring: ソースをリファクタリング

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,10 +3,64 @@
 import Link from 'next/link';
 import { FaBook, FaLaptopCode, FaGamepad, FaGithub } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
-
 import { Button } from './ui/button';
 import { ModeToggle } from '../components/ui/toggle';
 import { HumburgerMenu } from './HamburgerMenu';
+
+interface NavLink {
+  href: string;
+  icon: React.ElementType;
+  text: string;
+}
+
+interface SocialLink {
+  href: string;
+  icon: React.ElementType;
+}
+
+const NAV_LINKS: NavLink[] = [
+  { href: '/articles/tech', icon: FaLaptopCode, text: '技術記事' },
+  { href: '/articles/book', icon: FaBook, text: '読んだ本' },
+  { href: '/articles/hobby', icon: FaGamepad, text: '趣味' },
+];
+
+const SOCIAL_LINKS: SocialLink[] = [
+  { href: 'https://twitter.com/hukuryo_', icon: FaXTwitter },
+  { href: 'https://github.com/hukuryo', icon: FaGithub },
+];
+
+const Logo: React.FC = () => (
+  <Link href="/" className="font-bold text-2xl hover:opacity-70">
+    Hukuryo-no-Blog
+  </Link>
+);
+
+const NavLinks: React.FC = () => (
+  <div className="ml-5">
+    {NAV_LINKS.map(({ href, icon: Icon, text }) => (
+      <Link
+        key={href}
+        href={href}
+        className="text-sm px-3 ease-in-out duration-300 hover:opacity-70"
+      >
+        <Icon className="inline mr-1 mb-1" />
+        {text}
+      </Link>
+    ))}
+  </div>
+);
+
+const SocialButtons: React.FC = () => (
+  <>
+    {SOCIAL_LINKS.map(({ href, icon: Icon }) => (
+      <Link key={href} href={href} target="blank">
+        <Button variant="outline" size="icon" className="mx-1">
+          <Icon />
+        </Button>
+      </Link>
+    ))}
+  </>
+);
 
 export function Header() {
   return (
@@ -14,67 +68,19 @@ export function Header() {
       <div className="shadow-lg lg:flex lg:justify-center lg:items-center h-20 border border-gray-200 dark:border-gray-600 dark:bg-gray-700">
         <div className="items-center justify-between hidden lg:flex w-full mx-5">
           <div className="flex items-center">
-            <div className="font-bold text-2xl mb-1">
-              <Link href="/" className="hover:opacity-70">
-                Hukuryo-no-Blog
-              </Link>
-            </div>
-            <div className="ml-5">
-              <Link
-                href="/articles/tech"
-                className="text-sm px-3 ease-in-out duration-300 hover:opacity-70"
-              >
-                <FaLaptopCode className="inline mr-1 mb-1" />
-                技術記事
-              </Link>
-              <Link
-                href="/articles/book"
-                className="text-sm px-3 ease-in-out duration-300 hover:opacity-70"
-              >
-                <FaBook className="inline mr-1 mb-1" />
-                読んだ本
-              </Link>
-              <Link
-                href="/articles/hobby"
-                className="text-sm px-3 ease-in-out duration-300 hover:opacity-70"
-              >
-                <FaGamepad className="inline mr-1 mb-1" />
-                趣味
-              </Link>
-            </div>
+            <Logo />
+            <NavLinks />
           </div>
           <div>
-            <Link href="https://twitter.com/hukuryo_" target="blank">
-              <Button variant="outline" size="icon">
-                <FaXTwitter />
-              </Button>
-            </Link>
-            <Link href="https://github.com/hukuryo" target="blank">
-              <Button variant="outline" size="icon" className="mx-3">
-                <FaGithub />
-              </Button>
-            </Link>
+            <SocialButtons />
             <ModeToggle />
           </div>
         </div>
         <div className="lg:hidden">
           <div className="flex justify-between items-center h-full mt-6 mx-4">
-            <Link href="/" className="font-bold text-2xl mt-0">
-              <h1 className="font-bold text-2xl mt-0 hover:opacity-70">
-                Hukuryo-no-Blog
-              </h1>
-            </Link>
+            <Logo />
             <div>
-              <Link href="https://twitter.com/hukuryo_" target="blank">
-                <Button variant="outline" size="icon">
-                  <FaXTwitter />
-                </Button>
-              </Link>
-              <Link href="https://github.com/hukuryo" target="blank">
-                <Button variant="outline" size="icon" className="mx-3">
-                  <FaGithub />
-                </Button>
-              </Link>
+              <SocialButtons />
               <ModeToggle />
               <HumburgerMenu />
             </div>
