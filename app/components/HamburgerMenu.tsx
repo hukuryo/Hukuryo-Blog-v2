f69528c24527549e6b9f8abe674a205e5d261fa8
hukuryo <ryohei.1998.05.09@gmail.com>
2024-07-31 15:42:05 +0900

    💄 fixed: ダークモード時のデザインを修正

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -33,7 +33,7 @@ const MENU_ITEMS: MenuItem[] = [
 const MenuLink: React.FC<MenuItem> = ({ href, icon: Icon, text }) => (
   <Link
     href={href}
-    className="text-gray-700 py-2 mt-7 hover:bg-gray-200 transition duration-300 first:mt-4"
+    className="text-gray-700 dark:text-gray-300 py-2 mt-7 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 first:mt-4 pl-2"
   >
     <Icon className="mr-2 inline mb-1" />
     {text}
@@ -48,11 +48,13 @@ export function HamburgerMenu() {
           <IoIosMenu className="h-[1.0rem] w-[1.2rem]" />
         </Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="bg-white dark:bg-gray-800">
         <SheetHeader>
-          <SheetTitle>MENU</SheetTitle>
+          <SheetTitle className="text-gray-900 dark:text-gray-100">
+            MENU
+          </SheetTitle>
         </SheetHeader>
-        <nav className="text-left fixed z-10 sm:w-2/5 w-3/5 h-screen flex flex-col justify-start pt-2 ease-linear duration-300">
+        <nav className="text-left fixed z-10 sm:w-2/5 w-3/5 h-screen flex flex-col justify-start pt-2 ease-linear duration-300 bg-white dark:bg-gray-800">
           {MENU_ITEMS.map((item) => (
             <MenuLink key={item.href} {...item} />
           ))}
